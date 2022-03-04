144b4f1b2c2e3b48ee202057536d0c501ee1aab9
Long Nguyen <rongdat1710@gmail.com>
2022-03-04 10:14:36 -0500

    added screen home.js

--- services/service.js
+++ services/service.js
@@ -6,11 +6,16 @@ const apiKey = "api_key=94521603514044d897db51b0f6b9487d"
 //in react native, you always have to export a function to use them in another file
 //get popular movies
 export const getPopularMovies = async () => {
-    const resp = await axios.get(`${apiUrl}/movie/popular?${apiKey}&language=en-US&page=1`);
+    const resp = await axios.get(`${apiUrl}/movie/popular?${apiKey}`);
     return resp.data.results;
 }
 //get upcoming movie
 export const getUpcomingMovies = async () =>{
-    const resp = await axios.get(`${apiUrl}/movie/popular?${apiKey}&language=en-US&page=1`);
+    const resp = await axios.get(`${apiUrl}/movie/popular?${apiKey}`);
+    return resp.data.results;
+}
+//get popular tv show
+export const getPopularTv = async () =>{
+    const resp = await axios.get(`${apiUrl}/movie/popular?${apiKey}`);
     return resp.data.results;
 }
